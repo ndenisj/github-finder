2835f27fcd3d25429bbc2d833c51b0b5b5942ba6
Denis John <denis.john.ndarake@gmail.com>
2019-09-04 23:38:05 +0100

    Imported and nested Navbar and Users Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
