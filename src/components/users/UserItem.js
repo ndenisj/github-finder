22a6d4bf15090b7fc5175dd5d4268c771ff890c2
Denis John <denis.john.ndarake@gmail.com>
2019-09-08 02:49:51 +0100

    Added route to individual user profile page

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = props => {
-  const { login, avatar_url, html_url } = props.user;
+  const { login, avatar_url } = props.user;
   return (
     <div className="card text-center">
       <img
@@ -13,14 +14,9 @@ const UserItem = props => {
       />
       <h3>{login}</h3>
       <div>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={html_url}
-          className="btn btn-dark btn-sm my1"
-        >
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
